ecea702899b5d9251b42be30ed7bcd54e5a24cc2
Mayur Andulkar <mayurv.andulkar@gmail.com>
2019-01-10 22:17:38 +0100

    refact: Added code for actor and critic model

--- p3_collab-compet/maddpg_model.py
+++ p3_collab-compet/maddpg_model.py
@@ -4,18 +4,105 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-class Actor():
-    """"Model of the policy i.e. model mapping states to actions"""
-    def __init__(self):
-        pass
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
+
+
+class Actor(nn.Module):
+    """"Policy model i.e. model mapping states to actions"""
+    def __init__(self, state_size, action_size, seed=0, fc1_units=256, fc2_units=256):
+        """"
+        Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(Actor, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+        self.bn1 = nn.BatchNorm1d(fc1_units)
+        self.bn2 = nn.BatchNorm1d(fc2_units)
+        self.reset_parameters()
+
+    def forward(self, state):
+        """Actor network mapping states to actions"""
+        if state.dim() == 1:
+            state = torch.unsqueeze(state,0)
+        x = F.relu(self.fc1(state))
+        x = self.bn1(x)
+        x = F.relu(self.fc2(x))
+        x = self.bn2(x)
+        x = F.tanh(self.fc3(x))
+        return x
+
+    def reset_pararmeters(self):
+        """"Initialize weights with non-zero values"""
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
 class Critic():
     """Model estimating the Q-values or action-value function"""
-    def __init__(self):
-        pass
+    def __init__(self, state_size, action_size, seed=0, fc1_units=256, fc2_units=256):
+        """"
+        Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(Critic, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+        self.bn1 = nn.BatchNorm1d(fc1_units)
+        self.bn2 = nn.BatchNorm1d(fc2_units)
+        self.reset_parameters()
+
+    def reset_pararmeters(self):
+        """"Initialize weights with non-zero values"""
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, state):
+        """Critic network mapping states to action values"""
+        if state.dim() == 1:
+            state = torch.unsqueeze(state,0)
+        x = F.relu(self.fc1(state))
+        x = self.bn1(x)
+        x = F.relu(self.fc2(x))
+        x = self.bn2(x)
+        x = self.fc3(x) # No activation function here as we need to estimate the Q-value
+        return x
 
 class Actor_Critic_Models():
     """"Class containing all the models for a DDPG agent"""
-    def __init__(self):
-        pass
+    def __init__(self, n_agents, state_size, action_size, seed=0):
+        """
+        Params
+        ======
+            n_agents (int): number of agents
+            state_size (int): number of state dimensions for a single agent
+            action_size (int): number of action dimensions for a single agent
+            seed (int): random seed
+        """
+        self.actor_local = Actor(state_size, action_size, seed).to(device)
+        self.actor_target = Actor(state_size, action_size, seed).to(device)
+        critic_input_size = (state_size + action_size) * n_agents
+        self.critic_local = Critic(critic_input_size, seed).to(device)
+        self.critic_target = Critic(critic_input_size, seed).to(device)
